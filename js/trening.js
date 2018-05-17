239b8f18c10e0adcc1d441c23bdd50e6d95d8763
xMato <32490057+xMato@users.noreply.github.com>
2018-05-17 18:15:03 +0200

    Add files via upload

---
+++ js/trening.js
@@ -0,0 +1,45 @@
+function initmap() {
+
+    var directionsservice = new google.maps.directionsservice;
+
+    var inputstart = document.getelementbyid('start-input');
+    var searchboxstart = new google.maps.places.searchbox(inputstart);
+
+    var inputend = document.getelementbyid('end-input');
+    var searchboxend = new google.maps.places.searchbox(inputend);
+
+
+    document.getelementbyid('submitbutton').addeventlistener('click', function () {
+        var vzdialenost = 0;
+
+        directionsservice.route({
+            origin: inputstart.value,
+            destination: inputend.value,
+            travelmode: 'walking'
+        }, function (response, status) {
+            if (status === 'ok') {
+                vzdialenost = response.routes[0].legs[0].distance.value;
+                console.log(vzdialenost);
+                var theform = document.getelementbyid('formular');
+
+                var vzdialenostinput = document.createelement('input');
+                vzdialenostinput.value = vzdialenost;
+                vzdialenostinput.setattribute("name", "vzdialenost");
+                console.log(vzdialenostinput);
+                theform.appendchild(vzdialenostinput);
+
+                theform.submit();
+
+            } else {
+                window.alert('directions request failed due to ' + status);
+            }
+
+
+        });
+
+
+    });
+
+}
+
+

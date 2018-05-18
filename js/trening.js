76008d2e3f5a2922aee3cfbb363f398b2fe2371a
xMato <32490057+xMato@users.noreply.github.com>
2018-05-18 10:59:03 +0200

    Update trening.js
    zobrazovanie aj vzdialenosti pri vyhladavani

--- js/trening.js
+++ js/trening.js
@@ -30,62 +30,31 @@ function initMap() {
                 vzdialenostInput.setAttribute("hidden","true");
                 console.log(vzdialenostInput);
                 theForm.appendChild(vzdialenostInput);
-
                 theForm.submit();
 
             }else {
                 window.alert('Directions request failed due to ' + status);
             }
-
-
         });
-
-
-
     });
 
     var onChangeHandler = function() {
         if ((document.getElementById('start-input').value != '') && (document.getElementById('end-input').value != '')){
-            var latt = inputStart.value.geometry.location.lat();
-            var lngg = inputStart.value.geometry.location.lng();
+            //var latt = inputStart.value.geometry.location.lat();
+            //var lngg = inputStart.value.geometry.location.lng();
 
             var mapOptions = {
                 center: inputStart.value,
                 zoom: 16,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             }
-        directionsDisplay = new google.maps.DirectionsRenderer;
-        map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        directionsDisplay.setMap(map);
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+            map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            directionsDisplay.setMap(map);
 
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    }
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        }
 
-        var geocoder = new google.maps.Geocoder;
-
-        service.getDistanceMatrix({
-            origins: inputStart.value,
-            destinations: inputEnd.value,
-            travelMode: document.getElementById('mode').value,
-            unitSystem: google.maps.UnitSystem.METRIC,
-            avoidHighways: false,
-            avoidTolls: false
-        }, function(response, status) {
-            if (status !== 'OK') {
-                alert('Error was: ' + status);
-            } else {
-                var originList = response.originAddresses;
-                var destinationList = response.destinationAddresses;
-                var outputDiv = document.getElementById('output');
-                outputDiv.innerHTML = '';
-                var results = response.rows[0].elements;
-                outputDiv.innerHTML += originList[0] + ' to ' + destinationList[0] +
-                    ': ' + results[0].distance.text + ' in ' +
-                    results[0].duration.text + '<br>';
-
-
-            }
-        });
 
     };
 
@@ -93,12 +62,7 @@ function initMap() {
     document.getElementById('end-input').addEventListener('change', onChangeHandler);
 
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        /*if(document.getElementById('auto').checked){
-            var sposob= 'DRIVING';
-        }
-        else {
-            var sposob='WALKING';
-        }*/
+
         directionsService.route({
             origin: inputStart.value,
             destination: inputEnd.value,
@@ -106,6 +70,7 @@ function initMap() {
         }, function(response, status) {
             if (status === 'OK') {
                 directionsDisplay.setDirections(response);
+                document.getElementById('output').innerHTML += response.routes[0].legs[0].distance.value/1000 +"km";
             } else {
                 window.alert('Directions request failed due to ' + status);
             }
@@ -115,4 +80,3 @@ function initMap() {
     }
 
 }
-

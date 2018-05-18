07d082e708f44102d796fc99068a1cb985a8346d
Mondynko <34287967+Mondynko@users.noreply.github.com>
2018-05-18 22:50:54 +0200

    Add files via upload

---
+++ fillTable.js
@@ -0,0 +1,36 @@
+$(function() {
+    $.getJSON( "./getPaths.php", function( data ) {
+        loadTable(data.data);
+    });
+});
+
+function loadTable(data) {
+    console.log(JSON.parse(data));
+    var items = [];
+    var oneline;
+    $.each(JSON.parse(data), function (key, val) {
+        oneline = "<tr onclick='consolea("+val.id+")'><td>" + val.aktivnost + "</td><td>" + val.Vzdialenost + "</td><td>" + val.Mode + "</td>";
+        if(val.login) oneline += "<td>" + val.login + "</td>";
+        oneline += "</a></tr>";
+        items.push(oneline);
+    });
+
+    $("#tab-data").html(items.join(""));
+}
+
+var source = new EventSource('./getPaths.php');
+source.onmessage = function(event) {
+    loadTable(event.data);
+};
+
+function consolea(a){
+    location.href = "tabulkaTras.php?id="+ a;
+
+}
+
+function OverUrl(){
+    if (window.location.href.indexOf("id") > -1) {
+
+        showImport();
+    }
+}

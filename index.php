900a4fd1c613be2e18705bb98cab0f85fc12e7c8
xMato <32490057+xMato@users.noreply.github.com>
2018-05-18 21:17:23 +0200

    Update index.php
    vymazanie treningu

--- index.php
+++ index.php
@@ -75,7 +75,6 @@ session_start();
                 <?php
 
                 if(isset($_SESSION['logged'])) {
-                    echo "<li><a href='trening.php'>Tréning</a></li>";
                     echo "<li><a href='tabulkaTras.php'>Trasy</a>";
                     if($_SESSION['type'] == "admin") {
                         echo "<li><a href='users.php'>Užívateľia</a></li>";

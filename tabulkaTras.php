82a8c6c84d3f75f6c96f8bd32c171d1c89d1b2a6
xMato <32490057+xMato@users.noreply.github.com>
2018-05-18 19:25:00 +0200

    Add files via upload

--- tabulkaTras.php
+++ tabulkaTras.php
@@ -2,11 +2,59 @@
 
 require_once "config.php";
 session_start();
-
-if(!isset($_SESSION['logged'])) header("Location: index.php");
+/*
+if(!isset($_SESSION['logged']) || $_SESSION['type'] != "admin"){
+    header("Location: index.php");
+}*/
 
 $sql = "SELECT * FROM TRASA t LEFT JOIN USER u ON t.id_user=u.id LEFT JOIN USER_PATH up ON t.id=up.id_trasa";
 $result = $conn->query($sql);
+
+if(!isset($_SESSION['logged'])) header("Location: index.php");
+
+if(isset($_POST['start']) && isset($_POST['end'])) {
+
+    $origin =$_POST['start']; $destination = $_POST['end']; $vzdialenost = $_POST['vzdialenost'];
+
+    $start = $_POST['start'];
+    $end = $_POST['end'];
+    $mode = 0;
+    if(isset($_POST['mode'])) $mode = $_POST['mode'];
+    $id = $_SESSION['id'];
+    if (!mysqli_query($conn,"INSERT INTO TRASA (Start,End,Vzdialenost,Mode,id_user) VALUES ('$start','$end',$vzdialenost, $mode, $id)"))
+    {
+        echo("Error description: " . mysqli_error($con));
+    }else echo "Trasa bola úspešne pridaná";
+
+
+}
+
+if(isset($_POST['km']) ) {
+    $km = $_POST['km'];
+    $den = $_POST['den'];
+    $zaciatok = $_POST['zaciatok'];
+    $koniec = $_POST['koniec'];
+    $gps_zaciatok = $_POST['gps_zaciatok'];
+    $gps_koniec = $_POST['gps_koniec'];
+    $hodnotenie = $_POST['hodnotenie'];
+    $poznamka = $_POST['poznamka'];
+
+    if (empty($den)) $den = "NULL"; else $den = "'$den'";
+    if (empty($zaciatok)) $zaciatok = "NULL"; else $zaciatok = "'$zaciatok'";
+    if (empty($koniec)) $koniec = "NULL"; else $koniec = "'$koniec'";
+    if (empty($gps_zaciatok)) $gps_zaciatok = "NULL"; else $gps_zaciatok = "'$gps_zaciatok'";
+    if (empty($gps_koniec)) $gps_koniec = "NULL"; else $gps_koniec = "'$gps_koniec'";
+    if (empty($hodnotenie)) $hodnotenie = "NULL"; else $hodnotenie = "'$hodnotenie'";
+    if (empty($poznamka)) $poznamka = "NULL"; else $poznamka = "'$poznamka'";
+
+    $id = $_SESSION['id'];
+    if (!mysqli_query($conn, "INSERT INTO POKROKY (km,den,zcas,kcas,zgps,kgps,hodnotenie,poznamka,USER_ID) VALUES ('$km',$den,$zaciatok,$koniec,$gps_zaciatok,$gps_koniec,$hodnotenie,$poznamka,'$id')")) {
+        echo("Error description: " . mysqli_error($con));
+    } else
+        echo "Pokrok bol úspešne pridaný";
+
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -67,7 +115,7 @@ $result = $conn->query($sql);
 
                 <?php
                 if(isset($_SESSION['logged'])) {
-                    echo "<li><a href='trening.php'>Tréning</a></li>";
+                    //echo "<li><a href='trening.php'>Tréning</a></li>";
                     echo "<li class='active'><a href='tabulkaTras.php'>Trasy</a>";
                     if($_SESSION['type'] == "admin") {
                         echo "<li><a href='users.php'>Užívateľia</a></li>";
@@ -82,6 +130,79 @@ $result = $conn->query($sql);
     </nav>
     <!-- /Nav -->
 </header>
+<button class='main-btn' onclick='showLogin()' class='w3-button w3-green w3-large'>Pridaj Trasu</button>
+<button class='main-btn' onclick='showReg()' class='w3-button w3-green w3-large'>Zadaj Údaje</button>
+<!-- Trasa Modal -->
+<div id="id01" class="w3-modal">
+    <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+
+        <div id="map"></div>
+
+        <form id="formular" name="theForm" class="w3-container" method="post" action="tabulkaTras.php">
+            Start:
+            <input id="start-input" class="controls" type="text" name="start" required placeholder="Search Box">
+            <br>Koniec:
+            <input id="end-input" class="controls" type="text" name="end" required placeholder="Search Box">
+
+            <?php if(isset($_SESSION['logged']))
+                if($_SESSION['type'] == "admin") { ?>
+                    <select name="mode" class="controls">
+                        <option value="0">Privátny</option>
+                        <option value="1">Štafetový</option>
+                        <option value="2">Verejný</option>
+                    </select>
+                <?php } ?>
+            <div id="output">Dlzka: </div>
+
+            <input id="submitButton" class="w3-button w3-block w3-blue w3-section w3-padding" type="button" value="Ulož Trasu">
+            <div id="hidden_form_container" style="display:none;"></div>
+        </form>
+
+
+
+        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+            <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Zrušiť</button>
+        </div>
+
+    </div>
+</div>
+<!-- /Trasa Modal -->
+
+
+<!-- Udaje Modal -->
+<div id="id02" class="w3-modal">
+    <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+
+        <form class="w3-container" method="post" action="tabulkaTras.php">
+            <div class="w3-section">
+                <label><b>Prejdené km</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Zadaj počet prejdených km" name="km" required>
+                <label><b>Deň</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="date" placeholder="Zadaj deň" name="den" >
+                <label><b>Začiatok tréningu</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="time" placeholder="Zadaj začiatok tréningu" name="zaciatok" >
+                <label><b>Koniec tréningu</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="time" placeholder="Zadaj koniec tréningu" name="koniec"     >
+                <label><b>GPS Začiatku</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Zadaj GPS" name="gps_zaciatok" >
+                <label><b>GPS Konca</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Zadaj GPS" name="gps_koniec" >
+                <label><b>Hodnotenie</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Zadaj hodnotenie" name="hodnotenie" >
+
+                <label><b>Poznámka</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Zadaj poznámky" name="poznamka" >
+                <button class="w3-button w3-block w3-blue w3-section w3-padding" type="submit">Pridať</button>
+            </div>
+        </form>
+
+        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+            <button onclick="document.getElementById('id02').style.display='none'" type="button" class="w3-button w3-red">Zrušiť</button>
+        </div>
+
+    </div>
+</div>
+<!-- /Udaje Modal -->
 
 <!-- Import Modal -->
 <div id="id04" class="w3-modal">
@@ -109,6 +230,8 @@ $result = $conn->query($sql);
     </div>
 </div>
 <!-- /Import Modal -->
+
+
 
 <!-- Table -->
 <div class="container">
@@ -188,6 +311,9 @@ $result = $conn->query($sql);
 <script type="text/javascript" src="js/tableSort.js"></script>
 <script type="text/javascript" src="js/toPDF.js"></script>
 <script type="text/javascript" src="js/fillTable.js"></script>
+<script type="text/javascript" src="js/trening.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDX0hzrQujtmr0d6wVd_LimQhJ3FY6pjLM&libraries=places&callback=initMap"></script>
+<script type="text/javascript" src="js/trening.js"></script>
 
 
 </body>

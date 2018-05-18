881a585d0402d39cf799a8afaae9a0cae3fa24c0
gizdicex <32488241+gizdicex@users.noreply.github.com>
2018-05-18 19:24:27 +0200

    Add files via upload

--- tabulkaTras.php
+++ tabulkaTras.php
@@ -2,10 +2,8 @@
 
 require_once "config.php";
 session_start();
-/*
-if(!isset($_SESSION['logged']) || $_SESSION['type'] != "admin"){
-    header("Location: index.php");
-}*/
+
+if(!isset($_SESSION['logged'])) header("Location: index.php");
 
 $sql = "SELECT * FROM TRASA t LEFT JOIN USER u ON t.id_user=u.id LEFT JOIN USER_PATH up ON t.id=up.id_trasa";
 $result = $conn->query($sql);
